e08b334c7c1131d9808ae8c29f76fe7f235c01b3
pawelblicharz <pawel.blich@o2.pl>
2021-03-03 17:59:48 +0000

    portfolio source code

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  //unwind menu
+  $(".btn").click(function () {
+    $("nav ul").slideToggle(1000);
+  });
+//toggle button
+  $(".navbar-toggler").click(function () {
+    $(".navbar-toggler").toggleClass("change");
+  });
+//toggle button 2
+  $(".navbar-toggler2").click(function () {
+    $(".navbar-toggler2").toggleClass("changed");
+  });
+//photo slide
+  // $(".navbar-toggler2").click(function () {
+  //   $(".photo-container").toggleClass("move-down");
+  // });
+  $(".btn2").click(function () {
+    $("article .photo-container").slideToggle(1500);
+  });
+
+});
