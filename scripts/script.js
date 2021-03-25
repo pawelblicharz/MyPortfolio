159a3c46b4b77be271d8eaa2897e486959eaa781
pawelblicharz <pawel.blich@o2.pl>
2021-03-25 13:02:24 +0000

    add button animation and certificate

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,9 @@ $(document).ready(function () {
   $(".navbar-toggler").click(function () {
     $(".navbar-toggler").toggleClass("change");
   });
+  $(".btn-inner").click(function () {
+    $(".btn-inner").toggleClass("change");
+  });
 //toggle button 2
   $(".navbar-toggler2").click(function () {
     $(".navbar-toggler2").toggleClass("changed");
@@ -16,7 +19,7 @@ $(document).ready(function () {
   //   $(".photo-container").toggleClass("move-down");
   // });
   $(".btn2").click(function () {
-    $("article .photo-container").slideToggle(1500);
+    $("article .photo-container").slideToggle(1000);
   });
 
 });
